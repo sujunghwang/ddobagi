2f60dfc0a2f45e5c56d949411f2c457604da35b0
kjyk0835 <kjyk0835@naver.com>
2023-03-16 16:03:36 +0900

    chore: 배포 확인용 코드 수정 2

--- be/ddobagi/src/main/java/com/a608/ddobagi/DdobagiApplication.java
+++ be/ddobagi/src/main/java/com/a608/ddobagi/DdobagiApplication.java
@@ -8,7 +8,7 @@ public class DdobagiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DdobagiApplication.class, args);
-		System.out.println("test!");
+		System.out.println("test~!");
 	}
 
 }
