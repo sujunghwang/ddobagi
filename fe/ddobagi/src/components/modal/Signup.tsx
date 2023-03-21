515ec0f29a175df7dd95ca7ca1cdd81772fd2aaa
unknown <driculer@gmail.com>
2023-03-21 09:44:51 +0900

    Feat: 로그인 및 회원가입 구성 - 로컬 스토리지에 저장 후 상태 변경 '

--- fe/ddobagi/src/components/modal/Signup.tsx
+++ fe/ddobagi/src/components/modal/Signup.tsx
@@ -1,23 +1,286 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 import Input from "@mui/joy/Input";
+import ColorBtn from "../ColorBtn";
+import styles from "./Modal.module.scss";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import TextField from "@mui/material/TextField";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/RootReducer";
+import { useNavigate } from "react-router-dom";
 
+type Props = {
+  closeModal: Function;
+};
 
-type Props = {};
+function SignUp({ closeModal }: Props) {
+  //번역
+  const reduxLanguage = useSelector(
+    (state: RootState) => state.languageChange.language
+  );
+  //언어 선택을 위한 함수들
+  const [language, setLanguage] = useState<string>("");
+  const handleChange = (event: SelectChangeEvent) => {
+    setLanguage(event.target.value as string);
+  };
+  //
 
-function SignUp({}: Props) {
+  // 비밀번호 표기를 변경하는 함수들
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [showPassword2, setShowPassword2] = React.useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleClickShowPassword2 = () => setShowPassword2((show) => !show);
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+  //
+
+  //입력값을 위한 함수들
+  const [name, setName] = useState<string>("");
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const [birthDay, setBirthDay] = useState<Date | null>(null);
+  const [submitBirth, setSubmitBirth] = useState<number>(0);
+  const handleBirthDayChange = (date: Date | null) => {
+    setBirthDay(date);
+    if (date) {
+      const year = new Date(date).getFullYear();
+      setSubmitBirth(year);
+    }
+  };
+
+  const [id, setId] = useState<string>("");
+  const handleIdChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setId(event.target.value);
+  };
+
+  const [password, setPassword] = useState<string>("");
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const [confirmPassword, setComfirmPassword] = useState<string>("");
+  const handleConfirmPasswordChange = (
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
+    setComfirmPassword(event.target.value);
+  };
+  //
+  // 제출 여부를 확인
+  const [submitted, setSubmitted] = useState<boolean>(false);
+  //
+  //입력값을 제출하는 함수 - 회원가입 등록 후 동기처리해서 바로 로그인.
+  const navigate = useNavigate();
+  const navigateToCategory = () => {
+    navigate("/CategoryList");
+  };
+  const Log = () => {
+    setSubmitted(true);
+    const token = {
+      name: name,
+      language: language,
+      submitBirth: submitBirth,
+      id: id,
+      password: password,
+      confirmPassword: confirmPassword,
+    };
+    if (password !== confirmPassword) {
+      alert("비밀번호를 확인해주세요");
+      return;
+    }
+    const UserStr = JSON.stringify(token);
+    localStorage.setItem("user", UserStr);
+    navigateToCategory();
+    closeModal();
+  };
+
   return (
-    <div>
-      <div>이름</div>
-      <Input placeholder="이름을 입력하세요" />
-      <div>생년월일</div>
-      <Input placeholder="생년월일을 입력하세요" />
-      <div>언어</div>
-      <div>아이디</div>
-      <Input placeholder="아이디를 입력하세요" />
-      <div>비밀번호</div>
-      <Input placeholder="아이디를 입력하세요" />
-      <div>비밀번호 확인</div>
-      <Input placeholder="아이디를 입력하세요" />
+    <div className={styles.FContainer}>
+      <div className={styles.CContainer}>
+        <div className={styles.HContainer}>
+          <div className={styles.IContainer}>
+            <div>
+              {reduxLanguage === "CN"
+                ? "姓名"
+                : reduxLanguage === "VI"
+                ? "tên"
+                : "이름"}
+            </div>
+            <Input
+              error={submitted === true && name === "" ? true : false}
+              autoFocus
+              placeholder="Name"
+              value={name}
+              onChange={handleNameChange}
+              required
+            />
+          </div>
+        </div>
+        <div className={styles.HContainer}>
+          <div className={styles.IContainer}>
+            <div>
+              {reduxLanguage === "CN"
+                ? "语言"
+                : reduxLanguage === "VI"
+                ? "ngôn ngữ"
+                : "언어"}
+            </div>
+            <FormControl sx={{ minWidth: "100%" }} size="small">
+              <InputLabel id="select-label" className={styles.SelectLabel}>
+                Language
+              </InputLabel>
+              <Select
+                labelId="select-label"
+                id="simple-select"
+                error={submitted === true && language === "" ? true : false}
+                value={language}
+                label="Language"
+                onChange={handleChange}
+                required
+              >
+                <MenuItem value={"KR"}>한국어</MenuItem>
+                <MenuItem value={"CN"}>中文</MenuItem>
+                <MenuItem value={"VI"}>Tiếng Việt</MenuItem>
+              </Select>
+            </FormControl>{" "}
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          {reduxLanguage === "CN"
+            ? "出生日期"
+            : reduxLanguage === "VI"
+            ? "ngày sinh"
+            : "생년월일"}
+        </div>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer components={["DatePicker"]}>
+            <DatePicker
+              label="Select Birthday"
+              openTo="year"
+              views={["year"]}
+              sx={{ width: "100%" }}
+              value={birthDay}
+              onChange={handleBirthDayChange}
+              onError={(error, value) => {
+                <TextField helperText={error} required />;
+              }}
+            />
+          </DemoContainer>
+        </LocalizationProvider>
+      </div>
+      <div className={styles.IContainer}>
+        <div>
+          {reduxLanguage === "CN"
+            ? "帐户"
+            : reduxLanguage === "VI"
+            ? "tài khoản"
+            : "아이디"}
+        </div>
+        <Input
+          placeholder="ID"
+          value={id}
+          onChange={handleIdChange}
+          error={submitted === true && id === "" ? true : false}
+          required
+        />
+      </div>
+      <div className={styles.IContainer}>
+        <div>
+          {reduxLanguage === "CN"
+            ? "密码"
+            : reduxLanguage === "VI"
+            ? "mật khẩu"
+            : "비밀번호"}
+        </div>
+        <FormControl sx={{ width: "100%" }} variant="outlined" size="small">
+          <OutlinedInput
+            placeholder="Password"
+            id="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            value={password}
+            onChange={handlePasswordChange}
+            required
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </div>
+      <div className={styles.IContainer}>
+        <div>
+          {reduxLanguage === "CN"
+            ? "验证密码"
+            : reduxLanguage === "VI"
+            ? "Xác nhận lại mật khẩu"
+            : "비밀번호 확인"}
+        </div>
+        <FormControl sx={{ width: "100%" }} variant="outlined" size="small">
+          <OutlinedInput
+            placeholder="Confirm Password"
+            id="outlined-adornment-confirm-password"
+            type={showPassword2 ? "text" : "password"}
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+            required
+            error={
+              submitted === true && password !== confirmPassword ? true : false
+            }
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword2}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword2 ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>{" "}
+      </div>
+      <div className={styles.BtnMargin}>
+        <ColorBtn
+          content={
+            reduxLanguage === "CN"
+              ? "加入会员"
+              : reduxLanguage === "VI"
+              ? "tham gia thành viên"
+              : "회원가입"
+          }
+          color="#FF6B6B"
+          width="100%"
+          onClick={Log}
+        ></ColorBtn>
+      </div>
     </div>
   );
 }
--- fe/ddobagi/src/components/modal/UserInfoModal.tsx
+++ fe/ddobagi/src/components/modal/UserInfoModal.tsx
@@ -2,34 +2,71 @@ import React from "react";
 import Login from "./Login";
 import SignUp from "./Signup";
 import InfoEdit from "./InfoEdit";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Box from "@mui/material/Box";
+import Backdrop from "@mui/material/Backdrop";
+import CloseIcon from "@mui/icons-material/Close";
+import styles from "./Modal.module.scss";
 
 type Props = {
-  show: string;
   closeModal: Function;
   modalContent: string;
   setModalContent: Function;
+  modal: boolean;
+};
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: { xs: "60vw", md: "400px" },
+  bgcolor: "background.paper",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  borderRadius: "10px",
+  boxShadow: 24,
+  p: 4,
 };
 
 function UserInfoModal({
-  show,
   closeModal,
   modalContent,
   setModalContent,
+  modal,
 }: Props) {
   const exit = () => closeModal();
 
   return (
-    <div onClick={exit}>
-      <div onClick={(e) => e.stopPropagation()}>
-        <div onClick={exit}>X</div>
-        <div>또바기 로고</div>
-        {modalContent === "Login" && (
-          <Login setModalContent={setModalContent} />
-        )}
-        {modalContent === "SignUp" && <SignUp />}
-        {modalContent === "InfoEdit" && <InfoEdit />}
-      </div>
-    </div>
+    <Modal
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      open={modal}
+      onClose={exit}
+      closeAfterTransition
+      slots={{ backdrop: Backdrop }}
+      slotProps={{
+        backdrop: {
+          timeout: 500,
+        },
+      }}
+    >
+      <Fade in={modal}>
+        <Box sx={style}>
+          <div onClick={exit} className={styles.CloseBtn}>
+            <CloseIcon />
+          </div>
+          <img src="img/Logo_Nologin.png" alt="logo" className={styles.Logo} />
+          {modalContent === "Login" && (
+            <Login setModalContent={setModalContent} closeModal={closeModal} />
+          )}
+          {modalContent === "SignUp" && <SignUp closeModal={closeModal} />}
+          {modalContent === "InfoEdit" && <InfoEdit closeModal={closeModal} />}
+        </Box>
+      </Fade>
+    </Modal>
   );
 }
 
