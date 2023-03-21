e475d2c32e4bc350313f5ad4c598dafe315ce1b2
leehyeji <zzambbang@kakao.com>
2023-03-21 15:15:13 +0900

    feat: 마이페이지 오답률, 진행률 조회 추가

---
+++ be/ddobagi/src/main/java/com/a608/ddobagi/db/repository/ScriptRepository.java
@@ -0,0 +1,20 @@
+package com.a608.ddobagi.db.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.a608.ddobagi.db.entity.Script;
+
+/**
+ *packageName    : com.a608.ddobagi.db.repository
+ * fileName       : QuizRepository
+ * author         : modsiw
+ * date           : 2023/03/21
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/03/21        modsiw       최초 생성
+ */
+public interface ScriptRepository extends JpaRepository<Long, Script> {
+	Long countBy();
+}
