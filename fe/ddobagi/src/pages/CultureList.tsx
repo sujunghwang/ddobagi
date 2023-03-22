135ef264061b355ddf947ab2dbe11d65b7f7ba38
임성준 <darkeldd@naver.com>
2023-03-22 11:47:22 +0900

    feat:랜딩페이지 추가 및 풀페이지 적용 랜딩페이지 타입스크립트 적용 및 기능 구현 closes #S08P22A608-186, #S08P22A608-193

--- fe/ddobagi/src/pages/CultureList.tsx
+++ fe/ddobagi/src/pages/CultureList.tsx
@@ -1,8 +1,49 @@
 import React from 'react';
+import { Typography, Box, Button } from '@mui/material';
+import cultureimg from '../assets/korean_culture.jpg'
+import { useNavigate } from "react-router-dom";
+import CultureDetail from '../components/Culture/CultureDetail';
 
 function CultureList() {
+  const navigate = useNavigate();
+  const moveCulture = () => {
+    navigate("/cultureitem");
+  }
   return(
-    <div>CultureList
+    <div>
+      <Typography>
+        CultureList
+      </Typography>
+      <Box
+        sx={{
+          width:"100%",
+          height:"280px",
+          display:"flex",
+          justifyContent :"center",
+          backgroundColor: "primary.dark",
+        }}>
+        <Typography sx={{
+          display:"flex",
+          fontSize: "50px",
+          alignItems:"center",
+          fontFamily: "CookieRun-Regular",
+        }}>
+          한국 문화 학습
+        </Typography>
+      </Box>
+      <Box>
+        <Typography>
+          한국의 예술 문화
+        </Typography>
+        <Box>
+          <Box>
+
+          </Box>
+        </Box>
+      </Box>
+      <Button onClick={moveCulture}>
+        하이요
+      </Button>
     </div>
   )
 }
