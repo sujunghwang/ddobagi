080f5ac10571143b69046375c8ba4f6cc0ef8f05
choijiyeon <choiwldus@naver.com>
2023-03-22 12:52:42 +0900

    fix: 단어퀴즈 정답 여부 시 응답깂 변경

--- be/ddobagi/src/main/java/com/a608/ddobagi/api/dto/respoonse/UserQuizSaveResponseDto.java
+++ be/ddobagi/src/main/java/com/a608/ddobagi/api/dto/respoonse/UserQuizSaveResponseDto.java
@@ -4,6 +4,7 @@ import com.a608.ddobagi.api.dto.request.UserQuizSaveRequestDto;
 import com.a608.ddobagi.db.entity.Quiz;
 import com.a608.ddobagi.db.entity.User;
 import com.a608.ddobagi.db.entity.UserQuiz;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -12,20 +13,10 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Data
+@AllArgsConstructor
 public class UserQuizSaveResponseDto {
 
     private boolean isNowCorrected;
 
     private boolean isFirstCorrected;
-
-    private User user;
-
-    private Quiz quiz;
-
-    public UserQuizSaveResponseDto(UserQuiz userQuiz) {
-        this.isNowCorrected = userQuiz.isNowCorrected();
-        this.isFirstCorrected = userQuiz.isFirstCorrected();
-        this.user = userQuiz.getUser();
-        this.quiz = userQuiz.getQuiz();
-    }
 }
--- be/ddobagi/src/main/java/com/a608/ddobagi/api/service/QuizService.java
+++ be/ddobagi/src/main/java/com/a608/ddobagi/api/service/QuizService.java
@@ -72,7 +72,7 @@ public class QuizService {
                     .quiz(quiz)
                     .build());
 
-            return new UserQuizSaveResponseDto(modifyUserQuiz);
+            return new UserQuizSaveResponseDto(modifyUserQuiz.isNowCorrected(),modifyUserQuiz.isFirstCorrected());
         }else{
             // 새로운 문제일 경우
             // 제출한 단어 퀴즈 문제 정답 여부와 함께 유저퀴즈에 저장 후 return
@@ -83,7 +83,7 @@ public class QuizService {
                     .quiz(quiz)
                     .build());
 
-            return new UserQuizSaveResponseDto(userQuiz);
+            return new UserQuizSaveResponseDto(userQuiz.isNowCorrected(),userQuiz.isFirstCorrected());
         }
     }
 }
