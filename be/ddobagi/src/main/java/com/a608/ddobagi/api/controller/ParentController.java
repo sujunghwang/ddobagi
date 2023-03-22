53ce78f98f30ad74665d859f083d021cb75a1cb6
leehyeji <zzambbang@kakao.com>
2023-03-22 14:20:34 +0900

    feat: 부모페이지 학습률 오답률 기능 추가

---
+++ be/ddobagi/src/main/java/com/a608/ddobagi/api/controller/ParentController.java
@@ -0,0 +1,45 @@
+package com.a608.ddobagi.api.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.a608.ddobagi.api.dto.respoonse.UserProgressResponseDto;
+import com.a608.ddobagi.api.dto.respoonse.UserQuizReviewResponseDto;
+import com.a608.ddobagi.api.service.UserService;
+import com.a608.ddobagi.common.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ *packageName    : com.a608.ddobagi.api.controller
+ * fileName       : ParentController
+ * author         : modsiw
+ * date           : 2023/03/22
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/03/22        modsiw       최초 생성
+ */
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class ParentController {
+
+	private final UserService userService;
+
+	@GetMapping("/{userId}/parents/progress")
+	public ApiResponse<UserProgressResponseDto> getUserProgressForParents(@PathVariable Long userId) {
+		return new ApiResponse<>(userService.findUserProgressForParents(userId));
+	}
+
+	@GetMapping("/{userId}/parents/review")
+	public ApiResponse<List<UserQuizReviewResponseDto>> getUserQuizReviewForParents(@PathVariable Long userId) {
+		return new ApiResponse<>(userService.getUserQuizReviewForParents(userId));
+	}
+}
