32b6d79b2f8fabf01d52c796d718548f8aaa863f
sssungjooon <dellojoon7@gmail.com>
2023-03-24 16:45:46 +0900

    feat:문화 페이지 번역 및 swiper 적용 문화학습 페이지 상세 연결, 더미데이터 활용 closes #S08P22A608-187, #S08P22A608-184

---
+++ fe/ddobagi/src/components/Swiper/SwiperList.tsx
@@ -0,0 +1,156 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import Swiper core and required modules
+import { RootState } from "../../redux/RootReducer";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Card from '@mui/material/Card';
+import { Button, Box, Typography, CardActionArea, CardMedia, CardContent, Grid } from "@mui/material";
+import cultureTest from "../Swiper/cultureTest.json"
+
+// 문화 상세 사진
+// import dokdo from "../../assets/dokdopeng.jpg"
+// import game from "../../assets/game.jpg"
+// import kb from "../../assets/kyungbok.jpg"
+// import netflix from "../../assets/netflix.jpg"
+// import ddobak from "../../assets/말남아.png"
+
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+type CultureProp = {
+  cultureThumbnail?: string;
+  cultureTitle?: string;
+  cultureId?: number;
+  isCompleted?: boolean;
+  boxColor: string;
+  culturecategoryName?: string;
+}
+
+function SwiperList({
+  cultureThumbnail,
+  cultureTitle,
+  cultureId,
+  isCompleted,
+  boxColor,
+  culturecategoryName,
+ } : CultureProp) {
+
+  const getColorCode = (color: string): string => {
+    if (color === "red") {
+      return "#FF6B6B";
+    }
+    else if (color === "green") {
+      return "#6BCB77"
+    }
+    else if (color === "blue") {
+      return "#4D96FF"
+    }
+    else if (color === "yellow") {
+      return "#FFD93D"
+    }
+    // 다른 색상에 대한 처리
+    return "#FFE69A";
+  };
+  
+  const colorCode = getColorCode(boxColor);
+
+  //언어 변수
+  const language = useSelector(
+    (state: RootState) => state.languageChange.language
+  );
+  //
+
+  const navigate = useNavigate();
+  const moveCulture = () => {
+    navigate("/cultureitem");
+  }
+
+  const Slides = cultureTest
+
+  return (
+    <Box 
+      sx= {{
+        width:"80%",
+        height:"470px",
+        marginLeft:"10%",
+        backgroundColor: colorCode, 
+        borderRadius: "30px",
+        boxShadow: "inset 0px 10px 0px rgba(0, 0, 0, 0.25) ",
+        // overflow : "auto",
+        overflow : "hidden",
+        // margin : "1rem 5rem 5rem 5rem",
+
+      }}>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={20}
+        slidesPerView={1}
+        navigation
+        // pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+        style={{
+          marginTop: "30px",
+          marginLeft: "10px",
+          padding: "30px"
+        }}
+        breakpoints={{
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+        }}
+      >
+        {Slides.map((slide) => (
+          <SwiperSlide key={(slide.cultureId)}>
+            <Card
+              sx={{ maxWidth: 345, borderRadius:"20px" }}
+              onClick={moveCulture}
+            >
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="220"
+                  image={slide.thumbnail}
+                />
+                <CardContent
+                  sx={{
+                    height: "80px"
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      fontSize:"20px",
+                      fontFamily: "CookieRun-Regular",
+                      textOverflow: "ellipsis",
+                    }}
+                    gutterBottom
+                  >
+                    {language === "CN" ?
+                      slide.cultureContentQueryDtoList[1].title
+                    : language === "VI" ?
+                      slide.cultureContentQueryDtoList[2].title
+                    : slide.cultureContentQueryDtoList[0].title
+                    }
+                  </Typography>
+                </CardContent>
+              </CardActionArea> 
+            </Card>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
+  );
+}
+
+export default SwiperList;
