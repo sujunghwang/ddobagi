7b26089731edda9fd5084441dc6c848adf483c26
unknown <driculer@gmail.com>
2023-03-27 14:58:30 +0900

    Feat:마이페이지 * 유저 로그 확인 * 유저 통계 확인

--- fe/ddobagi/src/components/Charts/MypageCharts.tsx
+++ fe/ddobagi/src/components/Charts/MypageCharts.tsx
@@ -1,20 +1,33 @@
 import React, { useState } from "react";
 import CircleCharts from "./CircleCharts";
 import styles from "./Charts.module.scss";
-
-function MypageCharts() {
+interface Props {
+  data: {
+    viewedVideoCount: string;
+    recoredScriptCount: string;
+    studiedQuizCount: string;
+    schoolCategoryProgress: string;
+    homeCategoryProgress: string;
+    storeCategoryProgress: string;
+    playgroundCategoryProgress: string;
+    scriptProgress: string;
+    quizProgress: string;
+    cultureProgress: string;
+  }
+}
+function MypageCharts({ data }: Props) {
   return (
     <div className={styles.Background}>
       <div className={styles.UpperGroup}>
-        <CircleCharts value={10} name="집에서" />
-        <CircleCharts value={40} name="학교에서" />
-        <CircleCharts value={30} name="가게에서" />
-        <CircleCharts value={60} name="놀이터에서" />
+        <CircleCharts value={Number(data.schoolCategoryProgress)} name="집에서" />
+        <CircleCharts value={Number(data.homeCategoryProgress)} name="학교에서" />
+        <CircleCharts value={Number(data.storeCategoryProgress)} name="가게에서" />
+        <CircleCharts value={Number(data.playgroundCategoryProgress)} name="놀이터에서" />
       </div>
       <div className={styles.DownGroup}>
-        <CircleCharts value={70} name="대화 연습" />
-        <CircleCharts value={80} name="단어 연습" />
-        <CircleCharts value={90} name="문화 영상" />
+        <CircleCharts value={Number(data.scriptProgress)} name="대화 연습" />
+        <CircleCharts value={Number(data.quizProgress)} name="단어 연습" />
+        <CircleCharts value={Number(data.cultureProgress)} name="문화 영상" />
       </div>
     </div>
   );
--- fe/ddobagi/src/pages/MyPage.tsx
+++ fe/ddobagi/src/pages/MyPage.tsx
@@ -10,6 +10,20 @@ function MyPage() {
   const [modal, setModal] = useState<boolean>(false);
   const [modalContent, setModalContent] = useState<string>("");
   const closeModal = () => setModal(false);
+  // 마이페이지 정보를 수령합니다. 이하는 더미데이터.
+  const data = {
+    "viewedVideoCount": "2",
+    "recoredScriptCount": "3",
+    "studiedQuizCount": "4",
+    "schoolCategoryProgress": "80",
+    "homeCategoryProgress": "70",
+    "storeCategoryProgress": "90",
+    "playgroundCategoryProgress": "100",
+    "scriptProgress": "60",
+    "quizProgress": "10",
+    "cultureProgress": "30"
+  }
+
   return (
     <div>
       <div className={styles.Banner}>
@@ -19,7 +33,7 @@ function MyPage() {
         <BreadCrumbs />
       </div>
       <div className={styles.title}>"유저명"의 기록</div>
-      <UserLog />
+      <UserLog data={data} />
       <div className={styles.Btn}>
         <ColorBtn
           content="회원정보 수정"
@@ -32,16 +46,16 @@ function MyPage() {
         />
       </div>
       <div className={styles.title}>학습 진행도</div>
-      <MypageCharts />
+      <MypageCharts data={data} />
       <div className={styles.CheckList}>
         <div className={styles.Header}> 다시 풀기 </div>
-        <div style={{fontSize:"1.5rem"}}> 틀렸던 문제들을 다시 풀어보세요!</div>
+        <div style={{ fontSize: "1.5rem" }}> 틀렸던 문제들을 다시 풀어보세요!</div>
         <div className={styles.Btn}>
           <ColorBtn
             content="풀러 가기"
             color="#FFD93D"
             width="11.5rem"
-            onClick={() => {}}
+            onClick={() => { }}
           />
         </div>
       </div>
