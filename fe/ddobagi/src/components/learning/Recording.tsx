193211861e5889a9dc9911ddba327f535eefbb50
choijiyeon <choiwldus@naver.com>
2023-03-30 09:52:40 +0900

    feat: 녹음파일 wav로 변환 코드 추가

--- fe/ddobagi/src/components/learning/Recording.tsx
+++ fe/ddobagi/src/components/learning/Recording.tsx
@@ -9,6 +9,20 @@ import CheckIcon from '@mui/icons-material/Check';
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/RootReducer";
 
+
+const toWav = require('audiobuffer-to-wav');
+const audioContext = new (window.AudioContext)();
+const fileReader = new FileReader();
+
+async function convertWebmToWav(blob: Blob) {
+  const audioBuffer = await audioContext.decodeAudioData(await blob.arrayBuffer());
+  const wav = toWav(audioBuffer);
+
+  return new Blob([ new DataView(wav) ], {
+    type: 'audio/wav'
+  });
+}
+
 interface Props {
   situationId: number;
   scriptId: number;
@@ -27,16 +41,15 @@ const Recording = (props: Props) => {
 
   const [score, setScore] = useState<number>(0);  // 서버에서 반환된 점수를 저장합니다.
   const addAudioElement = async (blob: Blob) => {
+    const convertedBlob = await convertWebmToWav(blob);
     const formData = new FormData();
     formData.append("situation_id", situationId.toString());
     formData.append("user_id", userId.toString());
     formData.append("script_id", scriptId.toString());
-    formData.append("file", new File([blob], "recording.wav"));
-
-    console.log();
+    formData.append("file", new File([convertedBlob], "recording.wav"));
 
     try {
-      const url = URL.createObjectURL(blob);
+      const url = URL.createObjectURL(convertedBlob);
       setBlobUrl(url);
       const response = await axios.post(
         "http://j8a608.p.ssafy.io:8080/api/conversations/record",
